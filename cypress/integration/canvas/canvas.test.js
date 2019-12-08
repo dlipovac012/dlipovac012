f74d8545827ef3c4793d85920cf48f661a66cdb7
Dalibor <dlipovac012@gmail.com>
2019-12-08 11:23:28 +0100

    Added test for rendering canvas. Removed screenshots

--- cypress/integration/canvas/canvas.test.js
+++ cypress/integration/canvas/canvas.test.js
@@ -1,8 +1,18 @@
 describe('Canvas loading test - ', () => {
 
-	it('should load canvas', () => {
+	it('should render canvas', () => {
 		cy.visit('http://localhost:8080');
 
-		cy.get('section').contains('canvas');
+		cy.get('section').within(() => {
+			cy.get('canvas').should('be.visible');
+		});
+	});
+
+	it('should only render one canvas', () => {
+		cy.visit('http://localhost:8080');
+
+		cy.get('section').within(() => {
+			cy.get('canvas').its('length').should('eq', 1);
+		});
 	});
 });
